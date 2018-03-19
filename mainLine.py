1da7903e386b48794cbfa266b1369a9c3e166d74
CHECHI <412214410@qq.com>
2018-03-20 00:44:40 +0800

    update block

--- mainLine.py
+++ mainLine.py
@@ -11,5 +11,19 @@
 
 from flask import Flask, jsonify, request
 
-# Instantiate the server
+# initial the server
 app = Flask(__name__)
+
+@app.route('/register', methods=['POST'])
+def mine():
+    pass
+
+if __name__ == '__main__':
+    # from argparse import ArgumentParser
+
+    # parser = ArgumentParser()
+    # parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    # args = parser.parse_args()
+    # port = args.port
+
+    app.run(host='0.0.0.0', port=5000)
